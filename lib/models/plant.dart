8a63ed665032bef72b2c8444b256cfcc6a0bdd93
AT-Phillips <71952975+AT-Phillips@users.noreply.github.com>
2025-05-13 15:45:02 -0500

    Update plant.dart

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,37 +1,54 @@
-class Plant {
-  final int? id;
-  final String name;
-  final String species;
-  final String imagePath;
-  final String careInstructions;
+import 'package:flutter/material.dart';
+import '../helpers/database_helper.dart';
+import '../models/plant.dart';
 
-  Plant({
-    this.id,
-    required this.name,
-    required this.species,
-    required this.imagePath,
-    required this.careInstructions,
-  });
+class MyPlantsScreen extends StatefulWidget {
+  const MyPlantsScreen({super.key});
 
-  // Convert Plant to Map for DB
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'species': species,
-      'imagePath': imagePath,
-      'careInstructions': careInstructions,
-    };
+  @override
+  _MyPlantsScreenState createState() => _MyPlantsScreenState();
+}
+
+class _MyPlantsScreenState extends State<MyPlantsScreen> {
+  List<Plant> _plants = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPlants();
   }
 
-  // Create Plant from Map
-  factory Plant.fromMap(Map<String, dynamic> map) {
-    return Plant(
-      id: map['id'],
-      name: map['name'],
-      species: map['species'],
-      imagePath: map['imagePath'],
-      careInstructions: map['careInstructions'],
+  Future<void> _loadPlants() async {
+    final plants = await DatabaseHelper().getPlants();
+    setState(() {
+      _plants = plants;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Plants')),
+      body: _plants.isEmpty
+          ? const Center(child: Text('No plants yet.'))
+          : ListView.builder(
+              itemCount: _plants.length,
+              itemBuilder: (context, index) {
+                final plant = _plants[index];
+                return ListTile(
+                  leading: plant.imagePath.isNotEmpty
+                      ? Image.asset(
+                          plant.imagePath,
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                        )
+                      : const Icon(Icons.local_florist),
+                  title: Text(plant.name),
+                  subtitle: Text(plant.species),
+                );
+              },
+            ),
     );
   }
 }
